19bf097fa498dfbd7931b33c645e702ab282ea42
Jeandre <jbotha111@gmail.com>
2019-07-11 14:35:42 +0200

    Added xdp program dependecies.

--- installcommands.sh
+++ installcommands.sh
@@ -1,1 +1,6 @@
 #! /bin/sh
+sudo apt update
+sudo apt install clang llvm libelf-dev
+sudo apt install linux-tools-$(uname -r)
+sudo apt install linux-headers-$(uname -r)
+sudo apt install bcc bpfcc-tools
