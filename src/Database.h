11a116711266ab7b418115e8cd5a6a85206cd281
Sisa Khoza <u15034993@tuks.co.za>
2019-07-12 02:55:22 +0200

    C db driver updates
    Added the following functions:
    get_status_by_country_name(const char*)
    get_status_by_country_id(const char*)
    
    If the country does not exist a status of HIGH/-1 is returned by default

--- src/Database.h
+++ src/Database.h
@@ -21,6 +21,8 @@ class Database
         void insert_into_blacklist (const char*);
         void insert_into_whitelist (const char*);
         void insert_into_country (const char*, const char*, const char*);
+        int get_status_by_country_name(const char*);
+        int get_status_by_country_id(const char*);
 
     protected:
         Database ();
@@ -28,13 +30,17 @@ class Database
 
     private:
         const char *uri_str = "mongodb+srv://darknites:D%40rkN1t3s@defendr-1vnvv.azure.mongodb.net/test?retryWrites=true&ssl=true";
-   	mongoc_client_t *client;
-   	mongoc_database_t *database;
-   	mongoc_collection_t *collection;
-   	bson_t *command, reply, *insert;
-   	bson_error_t error;
-   	char *str;
-   	bool retval;
+   	    mongoc_client_t *client;
+   	    mongoc_database_t *database;
+   	    mongoc_collection_t *collection;
+        mongoc_cursor_t *cursor;
+   	    bson_t *bson, *query;
+   	    bson_error_t error;
+   	    char *str;
+   	    bool retval;
+        const int LOW = 1;
+	    const int MED = 0;
+	    const int HIGH = -1;
 };
 
 #endif
