e65c1481a3d7cc97cc1308c322f822d722b4708f
MCArrim <u15019854@tuks.co.za>
2019-07-15 09:15:39 +0200

    Installation

--- installcommands.sh
+++ installcommands.sh
@@ -4,3 +4,10 @@ sudo apt install clang llvm libelf-dev
 sudo apt install linux-tools-$(uname -r)
 sudo apt install linux-headers-$(uname -r)
 sudo apt install bcc bpfcc-tools
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-tk
+sudo pip3 install pymongo
+sudo pip3 install dnspython
+
+
