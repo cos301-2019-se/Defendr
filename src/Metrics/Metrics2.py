65a55f5f124bdb21720a4245df4e112488aad80c
Sisa Khoza <u15034993@tuks.co.za>
2019-07-17 21:18:06 +0200

    Partial integration of Metrics and Interface
    Moved Metrics to interface to make it easier for scripts to reference each other
    
    Still need to prevent premature shutdown in the python scrips

--- src/Metrics/Metrics2.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-new=2
-
-url="http://localhost:3000/d/L7frhkNWk/defendr-system-load?orgId=1"
-
-webbrowser.open(url,new=new);
--- src/Metrics/stop.sh
+++
@@ -1,4 +0,0 @@
-killall prometheus
-killall node_exporter
-killall grafana-server
-killall xterm
