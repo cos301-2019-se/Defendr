b5cc40d1cb1aa7dbc9a0bc58a2019e8414caefe1
Muhammed Ismail Carrim <u15019854@tuks.co.za>
2019-07-23 12:44:55 +0200

    Update installcommands.sh

--- installcommands.sh
+++ installcommands.sh
@@ -5,6 +5,8 @@ cd src/mongoDriver
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
+sudo apt-get install -y libcurl4
+sudo apt-get install -y cmake
 sudo apt-get install -y -y mongodb-org
 sudo apt-get install -y libmongoc-1.0-0
 sudo apt-get install -y libbson-1.0
@@ -17,7 +19,7 @@ mkdir cmake-build
 cd cmake-build
 cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
 make
-sudo make install -y
+sudo make install
 cd ../../../../
 sudo apt install -y clang llvm libelf-dev
 sudo apt install -y linux-tools-$(uname -r)
@@ -40,13 +42,13 @@ cd IP2Location-C-Library-master
 sudo autoreconf -i -v --force
 sudo ./configure
 sudo make
-sudo make install -y
+sudo make install
 cd data
 sudo perl ip-country.pl
 cd ../../
 make
 sudo chmod +x /Interfaces/Metrics/Prometheus/prometheus
-sudo chmod +x /Interfaces/Metrics/Prometheus/node_exporter
-sudo chmod +x /Interfaces/Metrics/Prometheus/grafana-server
+sudo chmod +x /Interfaces/Metrics/node_exporter/node_exporter
+sudo chmod +x /Interfaces/Metrics/Grafana/bin/grafana-server
 cd Interfaces
 python3 Login.py
