a87dde864b2ef8e120720a0ea9d9436e11eae8cb
Muhammed Carrim <u15019854@tuks.co.za>
2019-08-19 15:14:03 +0200

    Cleaned Testing Branch

--- run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo mount -t bpf bpf /sys/fs/bpf
-sudo chmod +x /Interfaces/Metrics/Prometheus/prometheus
-sudo chmod +x /Interfaces/Metrics/Prometheus/node_exporter
-sudo chmod +x /Interfaces/Metrics/Prometheus/grafana-server
-cd src/Interfaces
-python3 Login.py
--- src/Interfaces_v2/stat_test.py
+++
@@ -1,5 +0,0 @@
-from controller import controller
-
-xdp =  controller("../")
-data = xdp.get_stats()
-print(data)
