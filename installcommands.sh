9ae95f9b31cd5c2d3015c70f2dfc7bc67fd44041
Muhammed Carrim <u15019854@tuks.co.za>
2019-08-22 14:50:42 +0200

    Metrics Linked Correctly and update ic.sh

--- installcommands.sh
+++ installcommands.sh
@@ -38,11 +38,14 @@ sudo pip3 install pygments
 sudo pip3 install docutils
 sudo pip3 install cython
 sudo pip3 install kivy
+sudo pip3 install prometheus-client
+sudo pip3 install IP2Location
 sudo apt install -y gradle
 sudo apt install -y net-tools
 sudo apt install -y apache2
 sudo apt-get install -y libtool
 sudo apt-get install -y autoconf
+sudo apt-get install -y arptables
 cd src
 sudo rm -r IP2Location-C-Library-master
 unzip IP2Location-C-Library-master.zip
@@ -57,4 +60,11 @@ cd ../../
 make
 sudo ldconfig
 cd Interfaces_v2
+wget https://github.com/chrislim2888/IP2Location-Python/archive/master.zip
+unzip master.zip
+rm master.zip
+sudo IP2Location-Python-master/python3 setup.py build
+sudo python3 IP2Location-Python-master/setup.py install
+mv IP2Location-Python-master/bin/IP-COUNTRY.BIN Metrics
+rm -r IP2Location-Python-master
 python3 main.py
