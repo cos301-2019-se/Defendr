e03adf562a351d61dfe9cd396bc2d55b13a455ba
Ruslynn Appana <38566658+Ryperior@users.noreply.github.com>
2019-08-23 14:56:42 +0200

    Add files via upload

--- src/Interfaces_v2/test_file.py
+++ src/Interfaces_v2/test_file.py
@@ -13,11 +13,12 @@ class Test_Unit(unittest.TestCase):
     
     IPs = [None] *10
 
+    # Unit test to see if the controller is working and available.    
     def test_load(self):
         result = c.loadXdp()
         self.assertEqual(result,"success","Did not load Xdp") 
         f.write(" - Attempting to load xdp program" + " " + result + " \n" ) 
-
+    # Unit test to see if black list function is available and working.    
     def test_black1(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -25,14 +26,14 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(". - Attempting to blacklist IP " + ip + " "+ result + " \n" ) 
  
-
+    # Unit test to see if black list function is available and working.
     def test_black2(self):
        
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
             result = c.blacklistIP(ip)
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" ) 
-
+    # Unit test to see if black list function is available and working.
     def test_black3(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -40,7 +41,7 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
  
-
+    # Unit test to see if black list function is available and working.
     def test_black4(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -48,14 +49,14 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
  
-
+    # Unit test to see if black list function is available and working.
     def test_black5(self):
        
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
             result = c.blacklistIP(ip)
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
-
+    # Unit test to see if black list function is available and working.
     def test_black6(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -63,14 +64,14 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
  
-
+    # Unit test to see if black list function is available and working.
     def test_black7(self):
        
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
             result = c.blacklistIP(ip)
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
-
+    # Unit test to see if black list function is available and working.
     def test_black8(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -78,7 +79,7 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
  
-
+    # Unit test to see if black list function is available and working.
     def test_black9(self):
         
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
@@ -86,7 +87,7 @@ class Test_Unit(unittest.TestCase):
             self.assertEqual(result,"success", "Failed to blacklist IP" )
             f.write(" - Attempting to blacklist IP " + ip + " "+ result + " \n" )
  
-
+    # Unit test to see if black list function is available and working.
     def test_black10(self):
        
             ip = ".".join(map(str,(random.randint(0,255)for _ in range(4))))           
