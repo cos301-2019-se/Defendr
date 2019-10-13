ec0e117ae1dceeb40cb2ed91f87e3eadc30afd73
u15034993 <22478687+SisaKhoza@users.noreply.github.com>
2019-10-13 15:57:57 +0200

    Update installcommands.sh
    Remove kivy ppa repo

--- installcommands.sh
+++ installcommands.sh
@@ -5,7 +5,6 @@ sudo scite /etc/ld.so.conf
 cd src/mongoDriver
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get update 
 sudo apt-get install -y libcurl4
 sudo apt-get install -y cmake
