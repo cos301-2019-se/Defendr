a5ae985151b0dfa1e20cfa1ba6acaf490a46fe83
Sisa Khoza <u15034993@tuks.co.za>
2019-10-14 17:02:50 +0200

    installcommdands.sh update & remote monitoring app build
    Changed command to make bash write to fil instead of user
    Changed monitoring app library to build an app for remote test machine

--- installcommands.sh
+++ installcommands.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 sudo chown -R $USER
-sudo apt-get install -y scite
-sudo scite /etc/ld.so.conf
+sudo echo "include /usr/local/lib" >> /etc/ld.so.conf
 cd src/mongoDriver
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
