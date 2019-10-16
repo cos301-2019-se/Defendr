9c5802e82ea5b359289cefb9cb15a90cea93e8e1
Sisa Khoza <u15034993@tuks.co.za>
2019-10-16 08:31:43 +0200

    Updates to meet feedback
    Moved website link upwards
    Removed link to presentation video

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo mount -t bpf bpf /sys/fs/bpf
-sudo chmod +x /Interfaces/Metrics/Prometheus/prometheus
-sudo chmod +x /Interfaces/Metrics/Prometheus/node_exporter
-sudo chmod +x /Interfaces/Metrics/Prometheus/grafana-server
+sudo chmod +x /Interfaces_v2/Metrics/Prometheus/prometheus
+sudo chmod +x /Interfaces_v2/Metrics/Prometheus/node_exporter
+sudo chmod +x /Interfaces_v2/Metrics/Prometheus/grafana-server
 cd src/Interfaces_v2
-python3 main.py
+sudo python3 main.py
